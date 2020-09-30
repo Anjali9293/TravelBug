cc15b61ce50681810aaf5492aa67cced4ef4463b
Anjali Pant <pantanjali7@gmail.com>
2020-09-30 19:58:46 -0400

    local storage and search history added

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 var apiKey = "002d0ebbeamsh799a5b869925f5cp1155fcjsnb19952ec579f";
-
 var userInput = document.getElementById("search-Input");
 var searchButton = document.getElementById("searchButton");
-var firstInput =document.getElementById("firstIntput");
-
+var searchHistory = JSON.parse(localStorage.searchHistory || "[]");
 searchButton.addEventListener("click",searchCity);
 
 function searchCity(event){
   event.preventDefault();
   var input = userInput.value;
+  if(!input || input == "") return;
   console.log(input); 
+  searchHistory.push(input)
+  localStorage.searchHistory = JSON.stringify(searchHistory);
 
-  localStorage.input = input;
+  var node = document.createElement("li");
+  node.setAttribute("class", "list-group-item");
+//   var textnode = document.createTextNode(City);
+//   node.appendChild(textnode);
+  document.querySelector(".cities").appendChild(node);
 
-  firstInput.textContent=localStorage.input;
+  node.textContent=input;
 
   
 }
 
+function createSidebarFromHistory() {
+	searchHistory.forEach(function (cityName, i) {
+		var node = document.createElement("li");
+		node.setAttribute("class", "list-group-item");
+		var textnode = document.createTextNode(cityName);
+		node.appendChild(textnode);
+		document.querySelector(".cities")
+			.appendChild(node);
+	});
+}
+createSidebarFromHistory();
 // function weatherPage(){
 
 // }
