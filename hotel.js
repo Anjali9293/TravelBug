134bc1453b14dd05f328f2dc096e1051168e24f9
Anjali Pant <pantanjali7@gmail.com>
2020-10-01 14:23:08 -0400

    got hotel name to display on the page

--- hotel.js
+++ hotel.js
@@ -1,8 +1,17 @@
-
+var rapidAPIHeaders = {
+    "x-rapidapi-host": "hotels4.p.rapidapi.com",
+    "x-rapidapi-key": "bd4b60f585mshb932ee3918ef8d1p1f6ceajsn5f0b0da033ac"
+};
 var apiKey = "002d0ebbeamsh799a5b869925f5cp1155fcjsnb19952ec579f";
 var userInput = document.getElementById("search-Input");
 var searchButton = document.getElementById("searchButton");
 var searchHistory = getSearchHistory();
+var card = {
+    hotelName: document.querySelectorAll(".card-title"),
+    hotelAddress: document.querySelectorAll(".card-text"),
+    hotelRating: document.querySelectorAll(".rating"),
+    hotelLink: document.querySelectorAll(".link")
+}
 //This function is initiated by clicking the search button that takes user input.
 function searchCity(event) {
     event.preventDefault();
@@ -10,6 +19,7 @@ function searchCity(event) {
     if (!input || input == "") return;
     addToSearchHistory(input);
     createListItem(input);
+    getHotelData(input);
 }
 //This function pushes user inputs to serachhistory and add them to the local storage
 function addToSearchHistory(cityName) {
@@ -33,7 +43,25 @@ function createListItem(cityName) {
     document.querySelector(".cities").appendChild(node);
 }
 
+function getHotelData(city) {
+    $.ajax({
+        "async": true,
+        "crossDomain": true,
+        "url": "https://hotels4.p.rapidapi.com/locations/search?locale=en_US&query=" + city,
+        "method": "GET",
+        "headers": rapidAPIHeaders
+    }).done(function(response) {
+        console.log(response);
+        for (var i = 0; i < card.hotelName.length; i++) {
+            let hotel = response.suggestions[3];
+            console.log(hotel);
 
+            card.hotelName[i].textContent = hotel.entities[i].name;
+
+        }
+
+    });
+}
 
 createSidebarFromHistory();
 
