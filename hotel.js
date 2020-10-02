149e30389dd309ae96e830aa729dc619428f07d1
Anjali Pant <pantanjali7@gmail.com>
2020-10-01 22:37:57 -0400

    added tourist attraction to the weather page and hotel info on hotel page

--- hotel.js
+++ hotel.js
@@ -1,5 +1,5 @@
 var rapidAPIHeaders = {
-    "x-rapidapi-host": "hotels4.p.rapidapi.com",
+    "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
     "x-rapidapi-key": "bd4b60f585mshb932ee3918ef8d1p1f6ceajsn5f0b0da033ac"
 };
 var apiKey = "002d0ebbeamsh799a5b869925f5cp1155fcjsnb19952ec579f";
@@ -32,6 +32,7 @@ function getSearchHistory() {
 }
 //This function goes through an array of strings in local storage and calls createrlistItem function
 function createSidebarFromHistory() {
+    getHotelData(searchHistory[searchHistory.length-1]); // auto load the last search history
     searchHistory.forEach(createListItem);
 }
 //This function creates a li element in HTMl and appends it to cities ul tag
@@ -40,26 +41,48 @@ function createListItem(cityName) {
     node.setAttribute("class", "list-group-item");
     var textnode = document.createTextNode(cityName);
     node.appendChild(textnode);
+    node.addEventListener("click", function () {
+        handleSideBarOnClick(cityName);
+    });
     document.querySelector(".cities").appendChild(node);
 }
 
+function handleSideBarOnClick(name) {
+	getHotelData(name);
+}
+
 function getHotelData(city) {
+    $('.hotels').empty();
     $.ajax({
         "async": true,
         "crossDomain": true,
-        "url": "https://hotels4.p.rapidapi.com/locations/search?locale=en_US&query=" + city,
+        "url": "https://tripadvisor1.p.rapidapi.com/locations/search?location_id=1&limit=30&sort=relevance&offset=0&lang=en_US&currency=USD&units=km&query=" + city,
         "method": "GET",
         "headers": rapidAPIHeaders
     }).done(function(response) {
-        console.log(response);
-        for (var i = 0; i < card.hotelName.length; i++) {
-            let hotel = response.suggestions[3];
-            console.log(hotel);
-
-            card.hotelName[i].textContent = hotel.entities[i].name;
-
-        }
-
+        let hotels = response.data.filter(function(obj) {
+            return obj.result_type == "lodging";
+        }) 
+        $('.city').text(hotels[0].result_object.address_obj.city);
+        hotels.forEach(function(hotel) {
+            const details = hotel.result_object;
+            const review = hotel.review_snippet.snippet;
+            console.log(details);
+            $(`            
+                <div class="col-sm-5">
+                    <div class="card portfolioCard">
+                        <img src="${details.photo.images.medium.url}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${details.name}</h5>
+                            <p class="card-text">Hotel Address: ${details.address}</p>
+                            <p class="rating"> Star Rating: ${details.rating}</p>
+                            <p>Top Review: ${review}</p>
+                            <a target="_new" href="https://www.google.com/travel/hotels/${details.address_obj.city}?q=${details.name}+${details.address_obj.street1}" class="btn btn-primary link">Hotel Link</a>
+                        </div>
+                    </div>
+                </div>
+            `).appendTo('.hotels');
+        })
     });
 }
 
--- weather.js
+++ weather.js
@@ -1,16 +1,14 @@
 const currentDay = moment().format('L');
+var rapidAPIHeaders = {
+    "x-rapidapi-host": "trueway-places.p.rapidapi.com",
+    "x-rapidapi-key": "bd4b60f585mshb932ee3918ef8d1p1f6ceajsn5f0b0da033ac"
+};
 var apiKey = "e854039facmshf240de781c376e5p1fdb86jsn66c280925862";
 var WeatherAPIKey = "555f0f1cc17650cb7069ee6104be4ed1";
 var userInput = document.getElementById("search-Input");
 var searchButton = document.getElementById("searchButton");
 var searchHistory = getSearchHistory();
-var card = {
-    images: document.querySelectorAll(".card-image"),
-	date: document.querySelectorAll(".card-title"),
-	temp: document.querySelectorAll(".card-temp"),
-	humidity: document.querySelectorAll(".card-Humidity")
-}
-var apiData
+
 //This function is initiated by clicking the search button that takes user input.
 function searchCity(event) {
     event.preventDefault();
@@ -31,6 +29,7 @@ function getSearchHistory() {
 }
 
 function createSidebarFromHistory() {
+    getweatherdata(searchHistory[searchHistory.length-1]);
     searchHistory.forEach(createListItem);
 }
 
@@ -51,29 +50,67 @@ function handleSideBarOnClick(name) {
 
 function getweatherdata(name) {
     var forecastqueryURL = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + name + "&APPID=" + WeatherAPIKey;
-
+    $('.weather').empty();
     $.ajax({
         url: forecastqueryURL,
         method: "GET"
     })
     .then(function (response) {
-        apiData = response
-        for (var i = 0; i < card.date.length; i++) {
-            let forecastIndex = ((i + 1) * 8) - 4;
+        console.log(response);
+        $('.city').text(response.city.name);
+        getNearbyPlaces(response.city.coord.lat, response.city.coord.lon);
+        for (var i = 0; i < 5; i++) {
+            let forecastIndex = ((i + 1) * 8) - 5; // to get the midday index of each day
             let forecast = response.list[forecastIndex];
-            let imageUrl = "https://openweathermap.org/img/wn/" + forecast.weather[0].icon + ".png";
             let forecastDate = moment(currentDay, "L")
                 .add((i + 1), 'days')
                 .format('L');
 
-            card.date[i].textContent = forecastDate;
-            card.images[i].src = imageUrl;
-            card.temp[i].textContent = "Temperature:"+forecast.main.temp;
-            card.humidity[i].textContent = "Humidity:"+forecast.main.humidity;
+
+            $(`
+            <div class="col-weather-day">
+                <div class="card portfolioCard">
+                  <img src="https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png" class="card-image" style="margin: 0 10px;">
+                  <div class="card-body">
+                    <h5 class="card-title">${forecastDate}</h5>
+                    <p class="card-temp">Temperature:${forecast.main.temp}&#186 F</p>
+                  </div>
+                </div>
+              </div>  
+            `).appendTo('.weather');
         }
 
     });
 }
+
+function getNearbyPlaces(lat, lon) {  
+    $.ajax({
+        "url": `https://trueway-places.p.rapidapi.com/FindPlacesNearby?type=tourist_attraction&radius=5000&language=en&location=${lat},${lon}`,
+        "async": true,
+        "crossDomain": true,
+        "method": "GET",
+        "headers": rapidAPIHeaders
+    }).done(function (response) {
+        console.log(response);
+        const attractions = response.results;
+        attractions.forEach(function(attraction) {
+            $(`            
+                <div class="col-sm-6">
+                    <div class="card portfolioCard">
+                        <div class="card-body">
+                            <h5 class="card-title">${attraction.name}</h5>
+                            <p class="card-text">Address: ${attraction.address}</p>
+                            <p class="rating"> Phonenumber: ${attraction.phone_number || "N/A"}</p>
+                            <p>Type: ${attraction.types.join(', ')}</p>
+                            <a target="_new" href="${attraction.website}" class="btn btn-primary link">Attraction Link</a>
+                        </div>
+                    </div>
+                </div>
+            `).appendTo('.attractions');
+        })
+    });
+}
+
 
 createSidebarFromHistory();
 
